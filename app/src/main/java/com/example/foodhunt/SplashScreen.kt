0147cf2976905e5c20eb1027778fb3ec3d9b293d
Saral03 <saralhanda13@gmail.com>
2022-07-02 00:04:04 +0530

    Added animation to make splash screen more functionable

--- app/src/main/java/com/example/foodhunt/SplashScreen.kt
+++ app/src/main/java/com/example/foodhunt/SplashScreen.kt
@@ -3,13 +3,23 @@ package com.example.foodhunt
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
 
+lateinit var img_logo:ImageView
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        val intent=Intent(this@SplashScreen,MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        img_logo=findViewById(R.id.img_logo)
+        img_logo.alpha=0f
+        img_logo.animate().setDuration(1500).alpha(1f).withEndAction(){
+            val intent=Intent(this@SplashScreen,MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+
     }
 }
