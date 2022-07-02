33dfcb7599b03d44a27f857c0d5a430492913efc
Saral03 <saralhanda13@gmail.com>
2022-07-02 23:57:39 +0530

    Added functionality in registration page

--- app/src/main/java/com/example/foodhunt/Registration.kt
+++ app/src/main/java/com/example/foodhunt/Registration.kt
@@ -1,13 +1,34 @@
 package com.example.foodhunt
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
+lateinit var name_reg: EditText
+lateinit var email_reg: EditText
+lateinit var mobile_reg: EditText
+lateinit var delivery_reg: EditText
+lateinit var pass_reg: EditText
+lateinit var pass_confirm_reg: EditText
+lateinit var but_reg:Button
 class Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
         title="Register"
+        name_reg=findViewById(R.id.name_reg)
+        email_reg=findViewById(R.id.email_reg)
+        mobile_reg=findViewById(R.id.mobile_reg)
+        delivery_reg=findViewById(R.id.delivery_reg)
+        pass_reg=findViewById(R.id.pass_reg)
+        pass_confirm_reg=findViewById(R.id.pass_confirm_reg)
+        but_reg=findViewById(R.id.but_reg)
+        but_reg.setOnClickListener {
+            val intent=Intent(this@Registration,Registration_Show::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/foodhunt/Registration_Show.kt
+++ app/src/main/java/com/example/foodhunt/Registration_Show.kt
@@ -2,10 +2,23 @@ package com.example.foodhunt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
+lateinit var name_reg_tv:TextView
+lateinit var email_reg_tv:TextView
+lateinit var mobile_reg_tv:TextView
+lateinit var delivery_reg_tv:TextView
+lateinit var pass_reg_tv:TextView
+lateinit var pass_confirm_reg_tv:TextView
 class Registration_Show : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration_show)
+        name_reg_tv=findViewById(R.id.name_reg_tv)
+        email_reg_tv=findViewById(R.id.email_reg_tv)
+        mobile_reg_tv=findViewById(R.id.mobile_reg_tv)
+        delivery_reg_tv=findViewById(R.id.delivery_reg_tv)
+        pass_reg_tv=findViewById(R.id.pass_reg_tv)
+        pass_confirm_reg_tv=findViewById(R.id.pass_confirm_reg_tv)
     }
 }
